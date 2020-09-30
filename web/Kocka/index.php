7a3646d84e55d526df61cd852deb5685ec2b8b9b
Sallai András <andras@c16-tanar.egressy.lan>
2020-09-30 20:06:11 +0200

    Egyszerű kocka póker

---
+++ web/Kocka/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$k1 = rand(1, 6);
+$k2 = rand(1, 6);
+$k3 = rand(1, 6);
+$k4 = rand(1, 6);
+$k5 = rand(1, 6);
+
+printf("Human: %d %d %d %d %d<br>",
+  $k1, $k2, $k3, $k4, $k5);
+
+$m1 = rand(1, 6);
+$m2 = rand(1, 6);
+$m3 = rand(1, 6);
+$m4 = rand(1, 6);
+$m5 = rand(1, 6);
+
+printf("Machine: %d %d %d %d %d",
+  $m1, $m2, $m3, $m4, $m5);
