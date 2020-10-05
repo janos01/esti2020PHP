c36c7bcbf4987530f1b6fe94fc193a89522a414b
Sallai András <termih@gmail.com>
2020-10-05 20:22:41 +0200

    Switch gyakorlat; Pikód bekérő

---
+++ web/Pinkod/fel.php
@@ -0,0 +1,14 @@
+<?php
+
+$kod = $_POST['kod'];
+
+switch($kod) {
+    case "1234" :
+        print('Üdv Mária');
+        break;
+    case "4321":
+        print('Üdv Béla');
+        break;
+    default:
+        print('Hibás kód!!!');
+}
---
+++ web/Pinkod/index.php
@@ -0,0 +1,12 @@
+<?php
+
+echo <<<EOT
+<form action="fel.php" method="post">
+
+<label>Pinkód:</label>
+<input type="password" name="kod"><br>
+
+<button type="submit">Ellenőrzés</button>
+
+</form>
+EOT;
