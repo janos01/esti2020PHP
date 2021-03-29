23c01fec1b4e3be8dd2afde050840cc4868eb5ca
Sallai András <termih@gmail.com>
2021-03-29 19:01:42 +0200

    Adatbázis létrehozva

--- gyak/videoteka/draft/videoteka.sql
+++ gyak/videoteka/draft/videoteka.sql
@@ -11,3 +11,11 @@ create table filmek (
 	leiras varchar(255),
 	allapot int
 );
+
+insert into filmek 
+(cim, ev, leiras, allapot)
+values
+('A temény rabjai', 1994, '1946-ban egy Andy Dufresne nevű bankárt - noha makacsul hangoztatja ártatlanságát - kettős gyilkosság elkövetése miatt életfogytiglani börtönbüntetésre ítélnek.', 0),
+('Forrest Gump', 1994,  'A georgiai Savannah városka árnyas buszmegállójában különös mesemondó üldögél. ', 0),
+('Halálsoron', 1999,  'Paul Edgecomb börtönőrként szolgál a Cold Mountain fegyház siralomházában a múlt század harmincas éveiben.', 0);
+
